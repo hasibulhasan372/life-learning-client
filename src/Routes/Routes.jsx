df39623cedbe114bf2808ffb7fdc9962e995a703
HasibulHasan <hasibulhasan372@gmail.com>
2023-06-07 05:25:49 +0600

    classe page created

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 import Home from "../pages/Home/Home/Home";
+import Classes from "../pages/CLasses/Classes";
 
 const router = createBrowserRouter([
     {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
             {
                 path: "/",
                 element: <Home></Home>
+            },
+            {
+                path: "/classes",
+                element: <Classes></Classes>
             }
         ]
     },
--- src/pages/shared/Navbar.jsx
+++ src/pages/shared/Navbar.jsx
@@ -3,22 +3,24 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div className="navbar-con flex justify-between items-center bg-sky-50 h-12 md:h-30  shadow-sm">
-          <div>
-          <Link to="/" className="flex md:gap-3 items-center">
-                <img src="" alt="" />
-                <h2 className="logo-name">Life Learning</h2>
-            </Link>
-          </div>
-          <div className="md:space-x-6 lg:space-x-10 text-lg font-semibold  ">
-            <Link to='/' className="">Home</Link>
-            <Link to='/instructors' className="">Instructors</Link>
-            <Link to='/classes' className="">Classes</Link>
-            <Link to='/dashboard' className="">Dashboard</Link>
-          </div>
-          <div>
-          <Link to='/login' className="btn btn-sm  bg-gradient-to-r from-purple-600 to-red-500 text-white capitalize font-semibold">Login</Link>
-          </div>
+        <div className="navbar-con flex justify-between items-center bg-sky-50 h-12 md:h-30 lg:h-[70px]  shadow-sm">
+            <div>
+                <Link to="/" className="flex md:gap-3 items-center">
+                    <img src="" alt="" />
+                    <h2 className="logo-name">Life Learning</h2>
+                </Link>
+            </div>
+            <div className="">
+               <ul className="md:hover:transition md:hover:duration-500 md:space-x-6 lg:space-x-10  flex items-center ">
+               <li className="menu-bar">  <Link to='/'>Home</Link></li>
+                <li className=" menu-bar"> <Link to='/instructors' className="">Instructors</Link></li>
+                <li className=" menu-bar"><Link to='/classes' className="">Classes</Link></li>
+                <li className="menu-bar"> <Link to='/dashboard' className="">Dashboard</Link></li>
+               </ul>
+            </div>
+            <div>
+                <Link to='/login' className="btn btn-sm  bg-gradient-to-r from-purple-600 to-red-500 text-white capitalize font-semibold">Login</Link>
+            </div>
         </div>
     );
 };
