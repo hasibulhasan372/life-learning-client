fe1c9e383e8146499f4c1048b8fb29c0cdf68973
HasibulHasan <hasibulhasan372@gmail.com>
2023-06-14 11:59:04 +0600

    create become a intstructor section

--- src/pages/Home/BecomeInstructor/BecomeInstructor.jsx
+++ src/pages/Home/BecomeInstructor/BecomeInstructor.jsx
@@ -1,19 +1,26 @@
 import { Fade } from "react-awesome-reveal";
+import { Link } from "react-router-dom";
 
 
 const BecomeInstructor = () => {
     return (
         <Fade delay={1e3} cascade damping={1e-1}>
-        <div className="py-6 sm:py-8 md:py-10 lg:py-16">
-           <div className="content-con">
-               <div>
-                   <h2 className="text-lg sm:text-xl md:text3xl lg:text-4xl font-bold text-green-500 text-center">Become an Instructor</h2>
-               </div>
-              
-           </div>
+            <div className="py-6 sm:py-8 md:py-10 lg:py-16">
+                <div className="content-con">
 
-       </div>
-      </Fade>
+                    <div className=" flex flex-col-reverse lg:flex lg:flex-row items-center lg:gap-32">
+                        <img src="https://i.ibb.co/kKXXVHP/instructor-1x-v3.jpg" alt="" className="px-2" />
+                        <div className=" text-center lg:text-left lg:pr-24">
+                            <h2 className="text-lg sm:text-xl md:text3xl lg:text-4xl font-bold text-green-500">Become an Instructor</h2>
+                            <p className="text-lg lg:pt-6 ">Instructors from around the world teach millions of students on Life Learning. We provide the tools and skills to teach what you love.</p>
+                            <Link to='/instructors' className="mt-3 lg:mt-6 btn text-white capitalize text-lg bg-gradient-to-r from-purple-500 to-red-500">Visit Our Instructors</Link>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </Fade>
     );
 };
 
--- src/pages/Instructors/Instructors.jsx
+++ src/pages/Instructors/Instructors.jsx
@@ -12,7 +12,7 @@ const Instructors = () => {
             <div className="py-6 sm:py-8 md:py-10 lg:py-16">
             <div className="content-con">
                 <div>
-                    <h2 className="text-lg sm:text-xl md:text3xl lg:text-4xl font-bold text-green-500 text-center">Instructors</h2>
+                    <h2 className="text-lg sm:text-xl md:text3xl lg:text-4xl font-bold text-green-500 text-center">Best Instructors We Have</h2>
                 </div>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3  xl:grid-cols-4 justify-center items-center gap-6 mt-4 md:mt-8 lg:mt-12">
                     { loading ? <span className="loading loading-bars loading-lg mx-auto"></span> :
